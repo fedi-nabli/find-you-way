14b7cdefa7d6b8d82cf7f5cec7a89130728c7e51
Fedi Nabli <fedinabli@gmail.com>
2022-06-23 16:50:33 +0100

    Updated ultrason pins

--- backend/utils/ultrason.js
+++ backend/utils/ultrason.js
@@ -1,5 +1,5 @@
 import Sonar from 'raspi-hc-sr04';
-let sonar = new Sonar({ triggerPin: 4, echoPin: 31 });
+let sonar = new Sonar({ triggerPin: 11, echoPin: 13 });
  
 const ultrason = () => sonar.read((duration) => {
   let distance = 343.0 * duration / 10000 * .5;
